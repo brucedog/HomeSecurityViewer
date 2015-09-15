8b7df8525d5f53157168bb523001245a202560c2
Bryce Foster <bryce1337@gmail.com>
2015-09-15 16:13:58 -0500

    new service
    Created new service to handle camera so it can easily created into a
    window service

---
+++ CameraService/CameraService.cs
@@ -0,0 +1,27 @@
+﻿using Akka.Actor;
+using log4net;
+using Topshelf;
+
+namespace CameraService
+{
+    class CameraService
+    {
+        private readonly ILog _log = LogManager.GetLogger(typeof(CameraService));
+
+        public bool Start()
+        {
+            _log.Info("Service is starting");
+            ClusterSystem = ActorSystem.Create("camera");
+            return true;
+        }
+
+        protected ActorSystem ClusterSystem { get; set; }
+
+        public bool Stop()
+        {
+            _log.Info("Service is stopped");
+            ClusterSystem.Shutdown();
+            return true;
+        }
+    }
+}
---
+++ CameraService/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Xml.Schema;
+using log4net.Config;
+using Topshelf;
+
+namespace CameraService
+{
+    public class Program
+    {
+        static void Main()
+        {
+//            XmlConfigurator.ConfigureAndWatch(new FileInfo(".\\log4net.config"));
+//            var host = HostFactory.New(x =>
+//            {                
+//                x.Service<CameraService>(s =>
+//                {
+//                    s.ConstructUsing(name => new CameraService());
+//                    s.WhenStarted(tc =>
+//                    {
+//                        XmlConfigurator.ConfigureAndWatch(
+//                            new FileInfo(".\\log4net.config"));
+//                        tc.Start();
+//                    });
+//                    s.WhenStopped(tc => tc.Stop());
+//                });
+//
+//                x.RunAsLocalSystem();
+//                x.SetDescription("SampleService Description");
+//                x.SetDisplayName("SampleService");
+//                x.SetServiceName("SampleService");
+//            });
+//
+//            host.Run();
+
+            HostFactory.Run(
+                r =>
+                {
+                    r.Service<CameraService>(s =>                        //2
+                    {
+                        s.ConstructUsing(name => new CameraService());     //3
+                        s.WhenStarted(
+                            tc =>
+                            {
+                                var fileInfo = new FileInfo(".\\log4net.config");
+                                XmlConfigurator.ConfigureAndWatch(fileInfo);
+                                tc.Start();
+                            });              //4
+                        s.WhenStopped(tc => tc.Stop());               //5
+                    });
+                    r.RunAsLocalSystem();                            //6
+
+                    r.SetDescription("Sample Topshelf Host");        //7
+                    r.SetDisplayName("Stuff");                       //8
+                    r.SetServiceName("Stuff");
+                });
+        }
+    }
+}
